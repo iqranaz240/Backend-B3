dfb516a158cf84c8510dd4597f67ee2fe79e70fd
iqranaz240 <ha4587323@gmail.com>
2024-05-19 12:13:46 +0500

    connect to database mongoDb Atlas

---
+++ dbConnect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConn = process.env.mongo;
+
+const connectToDatabase = async function () {
+    console.log(dbConn)
+    try {
+        await mongoose.connect(dbConn);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+module.exports = connectToDatabase
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 8000;
 const getRoutes = require('./src/routes/getRoutes');
+let connectToDatabase = require("./dbConnect")
 
 // Middleware to parse JSON bodies and URL-encoded data
 app.use(express.json());
 
 // Apply routes after middleware
 app.use('/', getRoutes);
+
+mongoose.set("strictQuery", false);
+connectToDatabase();
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
