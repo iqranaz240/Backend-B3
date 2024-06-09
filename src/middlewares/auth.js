989763977aa1714a9801eab5118f42e2a96fc44a
--global <iqranaz240@gmail.com>
2024-06-09 12:45:26 +0500

    added jwt token verification -  authorization

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,2 +1,46 @@
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const jwtSecret = process.env.jwtSecret;
 
+const verifyToken = (req, res, next) => {
+    const token = req.headers['token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        if (!decoded.email) return res.status(401).json({ error: 'Invalid token' });
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+const checkAdmin = (req, res, next) => {
+    const token = req.headers['token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        const role = decoded.role;
+        if(role !== 'admin') {
+            return res.status(403).send({ auth: false, message: 'Unauthorized.' });
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+const checkValidUser = (req, res, next) => {
+    const token = req.headers['token'];
+    if (!token) return res.status(401).send({ auth: false, message: 'No token provided.' });
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        if(req.query.email !== decoded.email) {
+            return res.status(403).send({ auth: false, message: 'Unauthorized.' });
+        }
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token' });
+    }
+}
+
+module.exports = {verifyToken, checkAdmin, checkValidUser}
--- src/routes/getRoutes.js
+++ src/routes/getRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const cont = require('../controllers/getRoutes')
+const { verifyToken, checkAdmin, checkValidUser } = require('../middlewares/auth')
 
 router.get('/', cont.getRequest)
 
 router.post('/signup', cont.signup)
 router.post('/login', cont.login)
-router.get('/getUsers', cont.getUser)
-router.get('/getUserByEmail', cont.getUserByEmail)
-router.delete('/deleteUserByEmail', cont.deleteUserByEmail)
-router.patch('/updateUserByEmail', cont.updateUserByEmail)
-
+router.get('/getUsers', checkAdmin, cont.getUser)
+router.get('/getUserByEmail', checkValidUser, cont.getUserByEmail)
+router.delete('/deleteUserByEmail', checkValidUser, cont.deleteUserByEmail)
+router.patch('/updateUserByEmail', checkValidUser, cont.updateUserByEmail)
 
 module.exports = router
